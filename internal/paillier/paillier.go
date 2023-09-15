efd6078f35aebf6a02a3aa510541520b579058d0
hnamzian <namazian87@gmail.com>
2023-09-15 17:12:25 -0400

    Moved paillier package to internal directory

--- internal/paillier/paillier.go
+++ internal/paillier/paillier.go
@@ -1,18 +1,23 @@
 package homomorphic
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/coinbase/kryptology/pkg/paillier"
 )
 
 type PaillierKey struct {
-	Pk *paillier.PublicKey
-	Sk *paillier.SecretKey
+	Pk *paillier.PublicKey `json:"pk"`
+	Sk *paillier.SecretKey `json:"sk"`
 }
 
-func CreatePaillierKeypair() (*PaillierKey, error) {
-	pkjson, err := os.ReadFile("pk.json")
+func GeneratePaillierKey() (*paillier.PublicKey, *paillier.SecretKey, error) {
+	return paillier.NewKeys()
+}
+
+func CreatePaillierKeypair(path string) (*PaillierKey, error) {
+	pkjson, err := os.ReadFile(fmt.Sprintf("%s/pk.json", path))
 	if err != nil {
 		panic(err)
 	}
@@ -22,7 +27,7 @@ func CreatePaillierKeypair() (*PaillierKey, error) {
 		panic(err)
 	}
 
-	skjson, err := os.ReadFile("sk.json")
+	skjson, err := os.ReadFile(fmt.Sprintf("%s/sk.json", path))
 	if err != nil {
 		panic(err)
 	}
